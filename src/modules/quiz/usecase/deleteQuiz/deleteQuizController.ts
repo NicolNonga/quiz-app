a894334a073a92ae2efb9e0096ecaac586a0627c
nicodemos ngonga <nicodemos.ngonga@ideiasdinamicas.com>
2023-07-12 22:12:21 +0100

    feature:<> deleting a quiz

--- src/modules/quiz/usecase/deleteQuiz/deleteQuizController.ts
+++ src/modules/quiz/usecase/deleteQuiz/deleteQuizController.ts
@@ -1,26 +1,22 @@
 import { IController } from "../../../core/interface/IController";
 import { DeleteQuizUsecase } from "./deleteQuizUseCase";
-import {Request, Response, request} from 'express'
+import { Request, Response, request } from "express";
 
 export class DeleteQuizController implements IController<any, any> {
-    constructor( private readonly quizDeleteUseCase: DeleteQuizUsecase){
-        
-    }
-   
-   public async handle(request?: Request, response?: Response):Promise<Response> {
-       const {quiz_id} = request.params;
+  constructor(private readonly quizDeleteUseCase: DeleteQuizUsecase) {}
 
-      
-       const quizDeleteOrError = await this.quizDeleteUseCase.execute(
-         quiz_id
-       
-        )
-        console.log(quizDeleteOrError.getValue())
-       if(!quizDeleteOrError.isSuccess){
-        return response.status(400).send({message: quizDeleteOrError.error})
-       }
-       
-       return response.status(200).send({message: 'quiz deletado com sucesso'})
+  public async handle(
+    request?: Request,
+    response?: Response
+  ): Promise<Response> {
+    const { quiz_id } = request.params;
 
+    const quizDeleteOrError = await this.quizDeleteUseCase.execute(quiz_id);
+
+    if (!quizDeleteOrError.isSuccess) {
+      return response.status(400).send({ message: quizDeleteOrError.error });
     }
+
+    return response.status(200).send({ message: "quiz deletado com sucesso" });
+  }
 }
