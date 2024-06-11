c96e72d3aa47b2891796abe4e86cde39ccf92219
itgeste <itgeste@pop-os.localdomain>
2024-06-11 20:41:41 +0100

    feature: puntuaç

--- src/modules/quizSessionsUsers/usecase/quizSectionPuntuation/showQuizSectionPutuationUseCase.ts
+++ src/modules/quizSessionsUsers/usecase/quizSectionPuntuation/showQuizSectionPutuationUseCase.ts
@@ -1,12 +1,16 @@
 ;import { IuseCase } from "../../../core/interface/IUseCase";
 import { QuizSectionUserPuntuation, puntuationInterface } from "../../repository/quiz_section_user_puntuation_repository";
 
+export interface showSectionPutuation {
+    user_id: string,
+    quiz_section_id: string
+}
 export class ShowQuizSectionPuntuationUseCase implements IuseCase<any, any> {
       constructor(private quizSectionUserPuntuationRepository: QuizSectionUserPuntuation){
 
       }
 
-    async execute(data: Omit<puntuationInterface, 'puntuation'>) {
+    async execute(data: showSectionPutuation) {
       const  {user_id, quiz_section_id} = data;
        console.log(user_id, quiz_section_id)
        
