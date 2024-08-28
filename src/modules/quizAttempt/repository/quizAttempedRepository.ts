2ae55a20cdc6902a92c3236c97bce85382c8dcd4
itgeste <itgeste@pop-os.localdomain>
2024-08-28 18:29:41 +0100

    feature: add-yarn to start the project

--- src/modules/quizAttempt/repository/quizAttempedRepository.ts
+++ src/modules/quizAttempt/repository/quizAttempedRepository.ts
@@ -4,57 +4,52 @@ import { db } from "../../../utils/db.server";
 import { saveQuizAttemptedInterface, saveQuizAttemptedInterfaceRepo } from "../usecase/saveQuizAttemptByUser/saveQuizAttemptedByUserUseCase";
 
 export class QuizAttemptedRepository implements saveQuizAttemptedInterfaceRepo {
-  
-  public async  listByUserAndQuizSection(data: Omit<saveQuizAttemptedInterface, "option_id">): Promise<any> {
-     console.log("data",data)
-       try {
-        return    await  db.quiz_attempt.findMany({
 
-            where: {
-              AND: [
-                {
-                  quiz_section_id: data.quiz_section_id,
-                  user_id: data.user_id
-                }
-              ]
-      
-            },
+  public async listByUserAndQuizSection(data: Omit<saveQuizAttemptedInterface, "option_id">): Promise<any> {
+
+    try {
+      const quiz_attempt = await db.quiz_attempt.findMany({
+        include: {
+          quiz_section: {
             include: {
-              quiz_section:{
+              section_question: {
                 include: {
-                  section_question:{
+                  quiz_question: {
                     include: {
-                      quiz_question: {
-                        include: {
-                          quiz_option: {}
-                        }
-                      }
+                      quiz_option: {}
                     }
                   }
                 }
               }
             }
+          }
+        }
 
-           })
-       } catch (error) {
-        
-       }
+      })
+
+      const filterQuizAttemp = quiz_attempt.filter((quiz) => quiz.user_id === data.user_id && quiz.quiz_section_id === data.quiz_section_id)
+
+      return filterQuizAttemp
+      console.log(filterQuizAttemp)
+    } catch (error) {
+
+    }
   }
   async create(data: saveQuizAttemptedInterface): Promise<void> {
-    console.log(data)
+
     try {
-       await db.quiz_attempt.create({
-        data : {
-            user_id: data.user_id,
-            quez_option_id: data.option_id,
-            quiz_section_id: data.quiz_section_id
+      await db.quiz_attempt.create({
+        data: {
+          user_id: data.user_id,
+          quez_option_id: data.option_id,
+          quiz_section_id: data.quiz_section_id
         }
-       })
+      })
     } catch (error) {
       throw new Error("Falha ao cadastra.");
     }
 
   }
 
-  
+
 }
